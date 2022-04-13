63df2a9fbc1d4f41e0a903c91d038a312fc17c2a
HaiderMuhammad <103218645+HaiderMuhammad@users.noreply.github.com>
2022-04-13 21:02:51 +0300

    Add files via upload

---
+++ Q1.dart
@@ -0,0 +1,22 @@
+
+
+void main() {
+
+  dynamic hour = DateTime.now().hour;  
+  print(hour);
+
+}
+
+/*
+const variables must be created with a constant value and
+the constructor (DateTime) isn’t a const constructor
+
+
+The Soluation =======================
+
+void main() {
+
+ int hour = DateTime.now().hour;  
+print(hour);}
+
+*/
